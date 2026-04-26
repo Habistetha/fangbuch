9cc94f607010606a205f94bbd24374f894c1656d
Habistetha <sebastianthieme.st@gmail.com>
2026-04-26 08:40:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "asv-fangbuch-v2-1";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./logo-asv.png"];
+﻿const CACHE_NAME = "asv-fangbuch-v2-2";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./logo-asv.png", "./hintergrund-fangbuch.jpg"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => null));
   self.skipWaiting();
@@ -12,3 +12,4 @@ self.addEventListener("fetch", event => {
   if(event.request.method !== "GET") return;
   event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then(response => response || caches.match("./index.html"))));
 });
+
